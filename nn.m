21614c9c91be1b6810c213991133c30a329ff5a9
chuck <14jhwang@stu.edu.cn>
2017-04-21 13:33:40 +0800

    add trainingNN

--- nn.m
+++ nn.m
@@ -1,7 +1,11 @@
 %% 王俊皓 2014101027 神经网络作业
 
-%% 输入数据和部分参数
 clear;
+clc;
+
+%% 输入数据和部分参数
+
+fprintf('---- 第 1 阶段: 载入数据 ----\n\n');
 
 % 读入数据
 [X, y, y_raw, labels] = loadData('iris.txt', ' ');
@@ -9,11 +13,14 @@ clear;
 % 定义层数, 如3层, 分别有2,2,3个神经元可定义为[2,2,3]
 num_input = size(X, 2);
 num_output = size(y, 2);
-hidden_num_list = [3, ]; % 可修改隐藏层的层数及每层个数
+hidden_num_list = [3, 2]; % 可修改隐藏层的层数及每层个数
 unit_num_list = [num_input, hidden_num_list, num_output];
 
 % 学习速率
 ALPHA = 0.1;
+
+% 迭代次数
+ITERATION = 200;
 
 % 测试集比例
 test_ptage = 0.2;
@@ -36,83 +43,40 @@ X = X(sel,:); y = y(sel,:);
 Xtrain = X(1:m_train, :); ytrain = y(1:m_train, :);
 Xtest = X(m_train+1:end, :); ytest = y(m_train+1:end, :);
 
-
-unit_num_list
-
-
-%% 初始化权值和阈值
-
-% 层数 (减去输入层和输出层)
-num_hidden_layer = size(unit_num_list, 2) - 2;
-
-% 定义权重
-WEIGHT = cell(num_hidden_layer + 1, 1);
-
-for i = 1:size(WEIGHT, 1)
-    WEIGHT{i} = random_initialize([unit_num_list(i), unit_num_list(i + 1)], num_input);
+fprintf('神经网络层级结构:(输入层 -> 隐藏层 -> 输出层)\n');
+fprintf('%d', unit_num_list(1));
+for i = 2:size(unit_num_list, 2)
+    fprintf(' -> %d', unit_num_list(i));
 end
+fprintf('\n\n');
 
-% 定义阈值
-
-THETA = cell(num_hidden_layer + 1, 1);
-
-for i = 1:size(THETA, 1)
-    THETA{i} = random_initialize([unit_num_list(i + 1), 1], num_input);
-end
+fprintf('迭代次数: %d   学习速率: %.4f\n\n', ITERATION, ALPHA);
 
 
-% 定义误差
+%% 训练
 
-ERROR = cell(num_hidden_layer + 1, 1);
-ERROR_GRAD = cell(num_hidden_layer + 1, 1);
+fprintf('---- 第 2 阶段: 训练模型 ----\n\n');
 
-DELTA_WEIGHT = cell(num_hidden_layer + 1, 1);
-DELTA_THETA = cell(num_hidden_layer + 1, 1);
+fprintf('<按任意键开始训练>\n')
 
+pause;
 
-%% 定义OUTPUT
+[THETA WEIGHT ERROR] = trainingNN(Xtrain, ytrain, unit_num_list, ALPHA, ITERATION);
 
-OUTPUT = cell(size(unit_num_list, 2), 1);
-
-for i = 2:size(OUTPUT, 1)
-    OUTPUT{i} = zeros(unit_num_list(i), 1);
-end
+% 误差分析
+fprintf('\n误差:\n%.5f (第1次迭代) -> %.5f (第%d次迭代)\n\n', sum(ERROR(1,:) .^ 2), sum(ERROR(end,:) .^ 2), ITERATION);
 
 
-%% TEST
+% 准确度
+ERROR_X = 1:ITERATION;
+ERROR_Y = sum(ERROR .^ 2, 2);
 
-iteration = 10;
+plot(ERROR_X, ERROR_Y, '-');
+xlabel('迭代次数');
+ylabel('误差');
+title('误差分析');
 
-for i = 1:iteration      % 第i次迭代
-    fprintf('第%d次迭代...\n', i);
-    for j = 1:m     % 第j个样本
-        OUTPUT{1} = X(j, :)';
-        % 前向传播
-        for k = 1:size(OUTPUT, 1) - 1     % 第k层
-            OUTPUT{k + 1} = sigmoid(WEIGHT{k}' * OUTPUT{k} - THETA{k});
-        end
-        % 后向传播
-        for k = 1:size(THETA, 1)
-            ik = size(THETA, 1) - k + 1; % 反向
-            % ERROR
-            if ik == size(THETA, 1)
-                ERROR{ik} = y(j, :)' - OUTPUT{ik + 1};
-                ERROR_GRAD{ik} = OUTPUT{ik + 1} .* (1 - OUTPUT{ik + 1}) .* ERROR{ik};
-            else
-                ERROR_GRAD{ik} = OUTPUT{ik + 1} .* (1 - OUTPUT{ik + 1}) .* (WEIGHT{ik + 1} * ERROR_GRAD{ik + 1});
-            end
-            % DELTA
-            DELTA_WEIGHT{ik} = ALPHA * OUTPUT{ik} * ERROR_GRAD{ik}';
-            DELTA_THETA{ik} = ALPHA * THETA{ik} .* ERROR_GRAD{ik};
-        end
-        % 更新
-        for k = 1:size(THETA, 1)
-            WEIGHT{k} = WEIGHT{k} + DELTA_WEIGHT{k};
-            THETA{k} = THETA{k} + DELTA_THETA{k};
-        end
-    end
-end
-
+%% 预测
 
 
 
--- trainingNN.m
+++ trainingNN.m
@@ -1,6 +1,76 @@
 %% 训练神经网络
 % X, y, unit_num_list是网络的尺寸, ALPHA学习速度, iteration迭代次数
-function [WEIGHT THETA] = trainingNN(Xtrain, ytrain, unit_num_list, ALPHA, iteration)
+function [WEIGHT THETA ERROR] = trainingNN(Xtrain, ytrain, unit_num_list, ALPHA, iteration)
+
+%% 初始化权值和阈值
+
+% 层数 (减去输入层和输出层)
+num_hidden_layer = size(unit_num_list, 2) - 2;
+
+% 定义权重
+WEIGHT = cell(num_hidden_layer + 1, 1);
+
+for i = 1:size(WEIGHT, 1)
+    WEIGHT{i} = random_initialize([unit_num_list(i), unit_num_list(i + 1)], unit_num_list(1));
+end
+
+% 定义阈值
+
+THETA = cell(num_hidden_layer + 1, 1);
+
+for i = 1:size(THETA, 1)
+    THETA{i} = random_initialize([unit_num_list(i + 1), 1], unit_num_list(1));
+end
+
+
+% 定义误差
+ERROR = zeros(iteration, unit_num_list(end));
+ERROR_GRAD = cell(num_hidden_layer + 1, 1);
+
+DELTA_WEIGHT = cell(num_hidden_layer + 1, 1);
+DELTA_THETA = cell(num_hidden_layer + 1, 1);
+
+
+%% 定义OUTPUT
+
+OUTPUT = cell(size(unit_num_list, 2), 1);
+
+for i = 2:size(OUTPUT, 1)
+    OUTPUT{i} = zeros(unit_num_list(i), 1);
+end
+
+
+%% 训练
+
+for i = 1:iteration      % 第i次迭代
+    for j = 1:size(Xtrain, 1)     % 第j个样本
+        OUTPUT{1} = Xtrain(j, :)';
+        % 前向传播
+        for k = 1:size(OUTPUT, 1) - 1     % 第k层
+            OUTPUT{k + 1} = sigmoid(WEIGHT{k}' * OUTPUT{k} - THETA{k});
+        end
+        % 后向传播
+        for k = 1:size(THETA, 1)
+            ik = size(THETA, 1) - k + 1; % 反向
+            % ERROR
+            if ik == size(THETA, 1)
+                ERROR(i, :) = ytrain(j, :)' - OUTPUT{ik + 1};
+                ERROR_GRAD{ik} = OUTPUT{ik + 1} .* (1 - OUTPUT{ik + 1}) .* ERROR(i, :)';
+            else
+                ERROR_GRAD{ik} = OUTPUT{ik + 1} .* (1 - OUTPUT{ik + 1}) .* (WEIGHT{ik + 1} * ERROR_GRAD{ik + 1});
+            end
+            % DELTA
+            DELTA_WEIGHT{ik} = ALPHA * OUTPUT{ik} * ERROR_GRAD{ik}';
+            DELTA_THETA{ik} = ALPHA * THETA{ik} .* ERROR_GRAD{ik};
+        end
+        % 更新
+        for k = 1:size(THETA, 1)
+            WEIGHT{k} = WEIGHT{k} + DELTA_WEIGHT{k};
+            THETA{k} = THETA{k} + DELTA_THETA{k};
+        end
+    end
+    fprintf('第 %d 次迭代...... Error: %.5f \n', i, sum(ERROR(i, :) .^ 2));
+end
 
 
 
