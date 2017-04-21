7bdfd1e746935346cfdafeca4a86c74d68cc007f
chuck <14jhwang@stu.edu.cn>
2017-04-21 08:02:41 +0800

    add sigmoid, trainingNN, random_initialized, nn

--- random_initialize.m
+++ random_initialize.m
@@ -1,5 +1,8 @@
-function R = random_initialize(INPUT, F)
+%% 随机初始化参数函数, size是返回的尺寸, F是输入层的单元个数
+function R = random_initialize(size, F)
 
-R = (2.4 / F) .* INPUT;
+R = rand(size(1), size(2)); % 初始化0 - 1
+R = R .* 2 - 1;             % 范围变换到-1 - 1
+R = (2.4 / F) .* R;         % Haykin的公式
 
 end
---
+++ trainingNN.m
@@ -0,0 +1,7 @@
+%% 训练神经网络
+% X, y, unit_num_list是网络的尺寸, ALPHA学习速度, iteration迭代次数
+function [WEIGHT THETA] = trainingNN(X, y, unit_num_list, ALPHA, iteration)
+
+
+
+end
